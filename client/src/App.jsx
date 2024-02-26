f1de40f64722bd3349eacb2041d83059c765d13c
rqueaArequipa <ruben.quea@tecsup.edu.pe>
2024-02-26 12:37:19 -0500

    finished... v1.1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -40,7 +40,7 @@ function App() {
           onChange={(e) => setPost({ ...post, photo: e.target.files[0] })}
         />
         <button>Upload</button>
-        <img src="https://document-files-2024.s3.us-east-1.amazonaws.com/R.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIA2RA5WBJQZUM5QH2S%2F20240226%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240226T172401Z&X-Amz-Expires=3600&X-Amz-Signature=36aea1ff5f915a6c33b494a6142c01842337589576cb34c344e682e89257ca06&X-Amz-SignedHeaders=host&x-id=GetObject" alt="" />
+        <img src="#" alt="" />
       </form>
     </div>
   );
