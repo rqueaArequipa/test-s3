3ffd018a1f0e39729841a5c02c040ff3899e043d
rqueaArequipa <ruben.quea@tecsup.edu.pe>
2024-02-25 20:09:51 -0500

    Project Structure

---
+++ client/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [post, setPost] = useState({
+    title: "",
+    photo: null,
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("photo", post.photo);
+    const response = await axios.post(
+      "http://localhost:3000/upload",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+
+    console.log(response);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="title"
+          onChange={(e) => setPost({ ...post, title: e.target.value })}
+        />
+        <input
+          type="file"
+          name="photo"
+          id="photo"
+          onChange={(e) => setPost({ ...post, photo: e.target.files[0] })}
+        />
+        <button>Upload</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
